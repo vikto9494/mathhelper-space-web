61ca9b54f8af74d0a737e31cf67a713cf2218438
vkatsman <vikto9494@gmail.com>
2022-07-24 17:47:41 +0300

    new photos added

--- src/pages/home-page/sections/about-us-section/images-urls.ts
+++ src/pages/home-page/sections/about-us-section/images-urls.ts
@@ -6,15 +6,23 @@ const horizontalPhotosUrls = [
   require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/Vikto-h-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/VladMilshin-h-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/PashaDenisov-h-1.jpg"),
+
+  require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/NikitaGritsaenko-h-1.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/TimTitov-h-1.jpeg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/horizontal-photos/VladKashei-h-1.jpg"),
 ];
 const verticalPhotosUrls = [
   require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/IlyaKozlov-v-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/JuliaSmirnova-v-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/JuliaSmirnova-v-2.jpg"),
 
-  require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/OlegPavlovsky-v-1.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/OlegPavlovsky-v-1.jpg"), //todo: change (too much OlegPavlovsky)
   require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/Vikto-v-1.png"),
   require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/VladMilshin-v-1.jpg"),
+
+  require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/IlyaKozlov-v-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/DenisKorotchenco-v-1.jpeg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/vertical-photos/NikitaGritsaenko-v-1.jpg"),
 ];
 const standardPhotosUrls = [
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/KonstantinMorozov-s-1.jpg"),
@@ -22,7 +30,7 @@ const standardPhotosUrls = [
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/NadezhdaKatsman-s-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/novikov_f_a-s-1.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/OlegPavlovsky-s-1.jpg"),
-  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/OlegPavlovsky-s-2.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/OlegPavlovsky-s-2.jpg"), //todo: change (too much OlegPavlovsky)
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/Vikto-s-1.png"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/Vikto-s-2.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/VladMilshin-s-1.jpg"),
@@ -42,6 +50,19 @@ const standardPhotosUrls = [
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/PavelDenisov-s-1.png"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/PavelDenisov-s-2.jpg"),
   require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/Ruslan-s-1.jpg"),
+
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/KonstantinMorozov-s-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/KonstantinMorozov-s-2.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/NadezhdaKatsman-s-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/novikov_f_a-s-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/DenisKorotchenco-s-1.jpeg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/NikitaGritsaenko-s-1.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/TimTitov-s-1.jpeg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/TimTitov-s-2.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/VladMilshin-s-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/VladMosin-s-1.jpg"),  //todo: change (doubling)
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/VladKachei-s-1.jpg"),
+  require("../../../../assets/home-page-about-us-section/photo-grid/standard-photos/VladKachei-s-2.jpg"),
 ];
 
 export { verticalPhotosUrls, horizontalPhotosUrls, standardPhotosUrls };
